d71e4fda91835fef00fb9df435d0cc3c6e12669d
Sujit Ahirrao <sujitahirrao3@gmail.com>
2020-09-10 01:05:46 +0530

    Exclude tests and visualization

--- {{ cookiecutter.repo_name }}/setup.py
+++ {{ cookiecutter.repo_name }}/setup.py
@@ -2,7 +2,7 @@ from setuptools import find_packages, setup
 
 setup(
     name='{{ cookiecutter.repo_name }}',
-    packages=find_packages(exclude=['tests', 'visualization']),
+    packages=find_packages(exclude=['tests', '*visualization']),
     version='0.1.0',
     description='{{ cookiecutter.description }}',
     author='{{ cookiecutter.author_name }}',
