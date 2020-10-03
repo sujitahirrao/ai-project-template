e03fc5376f566d3122f96c76c285682cd76a5fa8
Sujit Ahirrao <sujitahirrao3@gmail.com>
2020-10-03 20:30:31 +0530

    Change package name, add long_desc

--- {{ cookiecutter.repo_name }}/setup.py
+++ {{ cookiecutter.repo_name }}/setup.py
@@ -1,9 +1,20 @@
+import os
 from setuptools import find_packages, setup
 
+
+# Utility function to read the README file.
+# Used for the long_description.
+def read(file_name):
+    return open(os.path.join(os.path.dirname(__file__),
+                             file_name)).read()
+
+
 setup(
-    name='{{ cookiecutter.repo_name }}',
-    packages=find_packages(exclude=['tests', '*visualization']),
-    version='0.1.0',
-    description='{{ cookiecutter.description }}',
-    author='{{ cookiecutter.author_name }}',
+    name="{{ cookiecutter.package_name }}",
+    packages=find_packages(exclude=["tests", "*visualization"]),
+    version="0.1.0",
+    description="{{ cookiecutter.description }}",
+    author="{{ cookiecutter.author_name }}",
+    author_email="{{ cookiecutter.author_email }}",
+    long_description=read("README.md"),
 )
